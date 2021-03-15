4581d20be196014754aa44c8eb34d7fb20b57bfa
carlo <littleark@gmail.com>
2021-03-15 14:26:27 +0000

    added pretest, upgraded packages and cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,8 @@
-// import commonjs from 'rollup-plugin-commonjs';
 import { uglify } from 'rollup-plugin-uglify';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-// import babel from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json';
-import {terser} from "rollup-plugin-terser";
-import * as meta from "./package.json";
+import {terser} from 'rollup-plugin-terser';
+import * as meta from './package.json';
 
 export default [
   {
@@ -32,7 +30,6 @@ export default [
       format: "umd",
       indent: false,
       name: "chrt",
-      //exports: 'named'
     },
   },
   {
@@ -53,54 +50,6 @@ export default [
       format: "umd",
       indent: false,
       name: "chrt",
-      // exports: 'named'
     },
   }
 ];
-/*
-const config = {
-  input: "index.js",
-  // external: Object.keys(meta.dependencies || {}).filter(key => /^chrt-/.test(key)),
-  output: {
-    file: `dist/${meta.name}.js`,
-    name: "chrt",
-    format: "umd",
-    indent: false,
-    extend: true,
-    exports: 'named',
-    banner: `// ${meta.homepage} v${meta.version} Copyright ${(new Date).getFullYear()} ${meta.author}`,
-    globals: Object.assign({}, ...Object.keys(meta.dependencies || {}).filter(key => /^chrt-/.test(key)).map(key => ({[key]: "chrt"}))),
-  },
-  plugins: [
-    json(),
-    commonjs(),
-    resolve(),
-    babel({
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**',
-      // sourceMaps: "both",
-      babelrc: false,
-    }),
-  ]
-};
-
-export default [
-  config,
-  {
-    ...config,
-    output: {
-      ...config.output,
-      file: `dist/${meta.name}.min.js`,
-    },
-    plugins: [
-      ...config.plugins,
-      uglify(),
-      terser({
-        output: {
-          preamble: config.output.banner
-        }
-      })
-    ]
-  },
-];
-*/
