3f42cd64f4ac4bce77b63e7ed7541672add6a2a0
carlo <littleark@gmail.com>
2021-07-12 14:12:17 +0100

    upgraded chrt-line

--- test/chrts/index.js
+++ test/chrts/index.js
@@ -3,3 +3,4 @@ export { default as columns } from './columns.js';
 export { default as scatterplot } from './scatterplot.js';
 export { default as casesItaly } from './cases-it.js';
 export { default as DotPlot } from './dotplot.js';
+export { default as stackedArea } from './stacked-area-chart.js';
