aeb7c42ce4ab6c5ba7f7586a1cd0520568320f7d
carlo <littleark@gmail.com>
2022-09-03 11:50:18 +0200

    removed cases-it test and upgraded packages and node

--- test/chrts/index.js
+++ test/chrts/index.js
@@ -1,7 +1,7 @@
 export { default as bars } from './bars.js';
 export { default as columns } from './columns.js';
 export { default as scatterplot } from './scatterplot.js';
-export { default as casesItaly } from './cases-it.js';
+// export { default as casesItaly } from './cases-it.js';
 export { default as DotPlot } from './dotplot.js';
 export { default as stackedArea } from './stacked-area-chart.js';
 export { default as stackedBars } from './stacked-area-bars.js';
