023caeb98696f879fc55104929dbc1a5b20ab538
carlo <littleark@gmail.com>
2024-02-13 13:35:10 +0100

    removed chrt-interactions

--- test/chrts/columns.js
+++ test/chrts/columns.js
@@ -1,19 +1,19 @@
-import * as chrt from '../../dist/chrt';
+import * as chrt from "../../dist/chrt";
 
 const data = [...new Array(10)].map((d, i) => {
   return {
     x: i,
     y: Math.random() * 100,
-    y1: Math.random() * 100
+    y1: Math.random() * 100,
   };
 });
 
-export default async function(container) {
-  console.log('BAR CHRT READY TO CREATE', container);
+export default async function (container) {
+  console.log("BAR CHRT READY TO CREATE", container);
 
-  const response = await fetch('./data/test.json?1');
+  const response = await fetch("./data/test.json?1");
   const json = await response.json();
-  console.log('JSON', json);
+  console.log("JSON", json);
 
   chrt
     .Chrt()
@@ -22,26 +22,11 @@ export default async function(container) {
     .data(data)
     //.x({ domain: [0, 10] })
     .y([0, 100])
+    .add(chrt.yAxis(5).setTickPosition("outside").hideAxis().color("#999"))
+    .add(chrt.xAxis(3).color("#999"))
+    .add(chrt.horizontalGrid(5).color("#ddd"))
     .add(
-      chrt
-        .yAxis(5)
-        .setTickPosition('outside')
-        .hideAxis()
-        .color('#999')
-    )
-    .add(chrt.xAxis(3).color('#999'))
-    .add(chrt.horizontalGrid(5).color('#ddd'))
-    .add(
-      chrt
-        .chrtColumns()
-        .fill('#fa5a51')
-        .width(1)
-        .color('#000')
-        .strokeWidth(1)
-        .add(
-          chrt.interactions()
-            .callback('mousemove', d => console.log('move', d))
-        )
+      chrt.chrtColumns().fill("#fa5a51").width(1).color("#000").strokeWidth(1),
     );
   // .add(
   //   new chrtColumns()
